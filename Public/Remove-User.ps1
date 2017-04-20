4698fb395b5800095e705fa099a0e618967413ce
Petter Bomban <petterbomban@gmail.com>
2017-04-20 10:45:55 +0200

    Remove-User working

--- Public/Remove-User.ps1
+++ Public/Remove-User.ps1
@@ -5,18 +5,31 @@ function Remove-User
     (
         [Parameter( Mandatory = $True,
                     ValueFromPipelineByPropertyName = $True)]
-        [string] $UserPrincipalName
+        [string] $Username,
 
-        
+        [Parameter( Mandatory = $False)]
+        [bool] $Confirm = $True
     )
 
     begin
     {
-
+        $ErrorActionPreference = "Stop"
+        Import-Module ActiveDirectory
     }
 
     process
     {
-
+        try
+        {
+            Write-Output "$Username -- deleting AD user.."
+            $User = Get-ADUser -Identity $Username -ErrorAction Stop
+            $Remove = $User | Remove-ADUser -Confirm:$Confirm -ErrorAction Stop
+            $Remove
+        }
+        catch
+        {
+            Write-Error "$Username -- could not remove user!"
+            Write-Error $Error[0]
+        }
     }
 }
